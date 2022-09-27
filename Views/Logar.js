e27769ccce4ac8ca074fa56d7b1d3e46fe88db82
GabrielVictor159 <gabrielvictor159487@gmail.com>
2022-09-27 10:37:34 -0300

    Refatorando os estilos da pagina Logar

--- Views/Logar.js
+++ Views/Logar.js
@@ -2,6 +2,7 @@ import React from "react";
 import {View, Image, Button, TouchableOpacity,Text, StyleSheet, TextInput} from "react-native";
 import Button1 from "../componentes/Button1";
 import { Link } from "@react-navigation/native";
+import LogarStyle from "../estilos/Views_Estilos/LogarStyle"
 export default class Logar extends React.Component{
     constructor(props){
         super(props);
@@ -12,50 +13,50 @@ export default class Logar extends React.Component{
     }
     render(){
         return(
-    <View style={styles.body}>
+    <View style={styles.LogarStyle.body}>
         <TouchableOpacity
-        style={styles.buttonBack}
+        style={styles.LogarStyle.buttonBack}
         onPress={() => this.props.navigation.goBack()}
         >
         <Image source={require("../assets/IconBack.png")}
         />
         </TouchableOpacity>
-        <Image style={styles.imageBack}
+        <Image style={styles.LogarStyle.ImageBack}
         source={require("../assets/Autism-bro.png")}
         />
-        <View style={{backgroundColor:'white', width:'100%', height:'80%' , top:10 ,borderTopRightRadius:50, borderTopLeftRadius:50, justifyContent:'center', alignItems:'center'}}>
-        <View style={{top:-120, alignItems:'center'}}>
+        <View style={styles.LogarStyle.ViewMiddle}>
+        <View style={styles.LogarStyle.InputContainer1}>
             
-            <Image style={{position:'relative', left:-80, top:22}} source={require("../assets/user.png")}/>
+            <Image style={styles.LogarStyle.InputIcons} source={require("../assets/user.png")}/>
             <TextInput 
-            style={{ width:220, textAlign:'center', borderBottomWidth : 1.0, borderColor:'#0D5692'}}
+            style={styles.LogarStyle.Input}
             placeholder={'E-mail'}
             onChangeText={
                 Usuario => {this.setState({Usuario})}
             }
             />
         </View>
-        <View style={{top:-80, alignItems:'center'}}>
+        <View style={styles.LogarStyle.InputContainer2}>
             
-            <Image style={{position:'relative', left:-80, top:22}} source={require("../assets/padlock.png")}/>
+            <Image style={styles.LogarStyle.InputIcons} source={require("../assets/padlock.png")}/>
             <TextInput 
-            style={{ width:220, textAlign:'center', borderBottomWidth : 1.0, borderColor:'#0D5692'}}
+            style={styles.LogarStyle.Input}
             placeholder={'Senha'}
             onChangeText={
                 Senha => {this.setState({Senha})}
             }
             />
         </View>
-        <View style={styles.ViewBottons}>
+        <View style={styles.LogarStyle.ViewBottons}>
         <Button1 width={220} height={55} borderRadius={20} navegacao='PaginaUsuario'  texto='Logar'/>
               <Link
-              style={styles.Link1}
+              style={styles.LogarStyle.Link1}
               to={{screen:'RecuperarSenha'}}
               >
               Esqueceu a senha?
               </Link>
               <Link
-              style={styles.Link2}
+              style={styles.LogarStyle.Link2}
               to={{screen:'Cadastrar'}}
               >
               Cadastrar-se
@@ -71,38 +72,5 @@ export default class Logar extends React.Component{
 }
 
 const styles = StyleSheet.create({
-    body:{
-        width:"100%",
-         height:"100%",
-          display:'flex',
-           flexDirection:'row',
-            justifyContent:'center',
-             alignItems:'flex-end',
-              backgroundColor:'#277BC0'
-    },
-    buttonBack:{
-        position:'absolute',
-         left:20,
-          top:50
-    },
-    imageBack:{
-        position:'absolute',
-         top:30 ,
-          left:'50%'
-    },
-    ViewBottons:{
-        top:70,
-        alignItems:'center'
-    },
-    Link1:{
-        color:'#277BC0',
-        fontSize:15,
-        top:10
-
-    },
-    Link2:{
-        color:'#277BC0',
-        fontSize:20,
-        top: 100
-    }
+  LogarStyle
 });
---
+++ estilos/Views_Estilos/LogarStyle.js
@@ -0,0 +1,67 @@
+const LogarStyle ={
+    body:{
+        width:"100%",
+         height:"100%",
+          display:'flex',
+           flexDirection:'row',
+            justifyContent:'center',
+             alignItems:'flex-end',
+              backgroundColor:'#277BC0'
+    },
+    buttonBack:{
+        position:'absolute',
+         left:20,
+          top:50
+    },
+    ImageBack:{
+        position:'absolute',
+         top:30 ,
+          left:'50%'
+    },
+    ViewMiddle:{
+        backgroundColor:'white',
+         width:'100%',
+          height:'80%' ,
+           top:25 ,
+           borderTopRightRadius:50,
+            borderTopLeftRadius:50,
+             justifyContent:'center',
+              alignItems:'center'
+    },
+    Input:{
+        width:220,
+         textAlign:'center',
+          borderBottomWidth : 1.0,
+           borderColor:'#0D5692'
+    },
+    InputContainer1:{
+        top:-120,
+        alignItems:'center'
+    },
+    InputContainer2:{
+        top:-80, 
+        alignItems:'center'
+    },
+    InputIcons: {
+        position:'relative',
+         left:-80,
+          top:22
+    },
+    ViewBottons:{
+        top:70,
+        alignItems:'center'
+    },
+    Link1:{
+        color:'#277BC0',
+        fontSize:15,
+        top:10
+
+    },
+    Link2:{
+        color:'#277BC0',
+        fontSize:20,
+        top: 100
+    }
+}
+
+export default LogarStyle;
