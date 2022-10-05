e1096b85397c166e2f9a30e55af3875004767235
GabrielVictor159 <gabrielvictor159487@gmail.com>
2022-10-05 18:13:23 -0300

    Mudando transição de telas iniciais

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Mensagem2 from './Views/Mensagem2';
 import Login from './Views/Login';
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator, TransitionPresets, CardStyleInterpolators } from '@react-navigation/stack';
 import Logar from './Views/Logar';
 import Cadastrar from './Views/Cadastrar';
 import {Image, TouchableOpacity} from 'react-native';
@@ -11,18 +11,39 @@ import RecuperarSenha from './Views/RecuperarSenha';
 import PaginaUsuario from './Views/PaginaUsuario';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import { GestureHandlerGestureEvent } from 'react-native-gesture-handler';
-const Stack = createNativeStackNavigator();
+import { fromLeft } from 'react-navigation-transitions';
+const animationConfig={
+  animation:'FadeInFromBottomAndroidSpec',
+  config:{
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 10,
+    restSpeedThreshold: 10,
+  }
+}
+const Stack = createStackNavigator();
 
 function App(navigation) {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Mensagem1">
+    <NavigationContainer
+    >
+      <Stack.Navigator initialRouteName="Mensagem1"
+      transitionConfig={()=>fromLeft(1000)}
+      >
         <Stack.Screen name="Mensagem1" component={Mensagem1} 
-        options={{ tabBarStyle: { display: "none" }, headerShown: false, tabBarShowLabel: false}}/>
+        options={{ tabBarStyle: { display: "none" }, headerShown: false, tabBarShowLabel: false, 
+        cardStyleInterpolator:CardStyleInterpolators.forHorizontalIOS,
+        }}/>
         <Stack.Screen name="Mensagem2" component={Mensagem2} 
-        options={{ tabBarStyle: { display: "none" }, headerShown: false, tabBarShowLabel: false}}/>
+        options={{ tabBarStyle: { display: "none" }, headerShown: false, tabBarShowLabel: false ,
+        cardStyleInterpolator:CardStyleInterpolators.forHorizontalIOS,
+        }}/>
         <Stack.Screen name='Login' component={Login}
-        options={{ tabBarStyle: { display: "none" }, headerShown: false, tabBarShowLabel: false}}/>
+        options={{ tabBarStyle: { display: "none" }, headerShown: false, tabBarShowLabel: false,
+        cardStyleInterpolator:CardStyleInterpolators.forHorizontalIOS,
+        }}/>
         <Stack.Screen name='Logar' component={Logar} 
         options={{ tabBarStyle: { display: "none" }, headerShown: false, tabBarShowLabel: false  }}/>
         <Stack.Screen name='Cadastrar' component={Cadastrar} 
--- configs/swipeConfig.js
+++ configs/swipeConfig.js
@@ -1,6 +1,6 @@
 const swipeConfig = {
     velocityThreshold: 0.1,
     directionalOffsetThreshold: 300,
-    gestureIsClickThreshold:20
+    gestureIsClickThreshold:5
   };
   export default swipeConfig;
